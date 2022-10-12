a4418cea79f9a5d9e767a06fb2e4f50a67d5783a
DA1YAYUAN <836919594@qq.com>
2022-10-12 09:18:56 +0800

    Assignment2 Commit

---
+++ IEMS5726 Data Science in Practice/IEMS5726_Assignment2_1155182964.py
@@ -0,0 +1,61 @@
+# <Your student ID>
+import numpy as np
+import pandas as pd
+# Problem 2
+def problem_2(filename,name):
+    # write your logic here, df is a dataframe
+    df = 0
+
+    return df
+
+# Problem 3
+def problem_3(filename,k):
+    # write your logic here, pc is a numpy array
+    pc = 0
+
+    return pc
+
+# Problem 4
+def problem_4(sentence):
+    # write your logic here
+    output = []
+
+    return output
+
+# Problem 5
+def problem_5(doc):
+    # write your logic here, df is a dataframe, instead of number
+    df = 0
+
+    return df
+
+# Problem 6
+def problem_6(image_filename):
+    # write your logic here, keypoint and descriptor are BRISK object
+    keypoint = 0
+    descriptor = 0
+
+    return keypoint, descriptor
+
+# Problem 7
+def problem_7(image1_filename, image2_filename):
+    # write your logic here, common_descriptor is the common desc.
+    common_descriptor = 0
+
+    return common_descriptor
+
+# Problem 8
+def problem_8(audio_filename, sr, n_mels, n_fft):
+    # write your logic here, spec is a tensor
+    spec = 0
+ 
+    return spec
+
+# Problem 9
+def problem_9(spec, max_mask_pct, n_freq_masks, n_time_masks):
+    # write your logic here
+    aug_spec = spec
+ 
+    return aug_spec
+
+
