e5c9d13dbb4b4c84a38565dca8a08b40510e0203
DA1YAYUAN <836919594@qq.com>
2022-10-28 13:27:46 +0800

    Problem_2 Half Commit

--- IEMS5726 Data Science in Practice/IEMS5726_Assignment3_1155182964.py
+++ IEMS5726 Data Science in Practice/IEMS5726_Assignment3_1155182964.py
@@ -21,16 +21,44 @@ def problem_2(filename, predictors, target):
     batch_size, learning_rate = 10, 0.01
     # load file
     df = pd.read_csv(filename)
+    df = df.iloc[1:]
     # set manual seed
     torch.manual_seed(5726)
-    x_train, x_test, y_train, y_test = train_test_split()
-    print(df.to_string())
+    x = df[predictors]
+    y = df[target]
+    # split data
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=5726)
+    # normalize Data
+    sc = StandardScaler()
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.fit_transform(x_test)
+    # 转为tensor
+    x_train = torch.FloatTensor(x_train)
+    y_train = torch.FloatTensor(y_train.to_numpy())
+    # 序列初始化模型 输出层 - hidden层- 输出层
+    model = nn.Sequential(nn.Linear(len(predictors), 5), nn.ReLU(), nn.Linear(5, 3), nn.ReLU(), nn.Linear(3, 1),
+                          nn.Sigmoid())
+    loss_function = nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+    # train model
+    print("===> start training...")
+    losses = []
+    for epoch in range(5000):
+        pred_y = model(x_train)  # Forward propagation
+        pred_y = pred_y.squeeze(-1)
+        loss = loss_function(pred_y, y_train)
+        losses.append(loss.item())
+        model.zero_grad()
+        loss.backward()  # Backward propagation
+        optimizer.step()  # Gradient descent
+    print("===> training end.")
     return model, test_precision, test_recall
 
 
 print(problem_2("IEMS5726_Assignment3_Data/winequality-white-binary.csv",
-                ["fixed acidity", "volatile acidity", "citric acid", "residual", "sugar", "chlorides",
-                 "free sulfur dioxide", "total sulfur", "dioxide", "density", "pH", "sulphates", "alcohol"],
+                ["fixed acidity", "volatile acidity", "citric acid", "residual sugar", "chlorides",
+                 "free sulfur dioxide", "total sulfur dioxide", "density", "pH", "sulphates", "alcohol"],
                 "quality"))
 
 
