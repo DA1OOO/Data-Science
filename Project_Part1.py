8b44b921d139ce63cc1899acaf13e645d210c157
DA1YAYUAN <836919594@qq.com>
2022-12-03 18:31:39 +0800

    1

--- Project_Part1.py
+++ Project_Part1.py
@@ -64,72 +64,71 @@ def padding_(sentences, seq_len):
     return features
 
 
-class SentimentRNN(nn.Module):
-    def __init__(self, no_layers, vocab_size, hidden_dim, embedding_dim, drop_prob=0.5):
-        super(SentimentRNN, self).__init__()
-
-        self.output_dim = output_dim
-        self.hidden_dim = hidden_dim
-
-        self.no_layers = no_layers
-        self.vocab_size = vocab_size
-
-        # embedding and LSTM layers
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-
-        # lstm
-        self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=self.hidden_dim,
-                            num_layers=no_layers, batch_first=True)
-
-        # dropout layer
-        self.dropout = nn.Dropout(0.3)
-
-        # linear and sigmoid layer
-        self.fc = nn.Linear(self.hidden_dim, output_dim)
-        self.sig = nn.Sigmoid()
-
-    def forward(self, x, hidden):
-        batch_size = x.size(0)
-        # embeddings and lstm_out
-        embeds = self.embedding(x)  # shape: B x S x Feature   since batch = True
-        # print(embeds.shape)  #[50, 500, 1000]
-        lstm_out, hidden = self.lstm(embeds, hidden)
-
-        lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
-
-        # dropout and fully connected layer
-        out = self.dropout(lstm_out)
-        out = self.fc(out)
-
-        # sigmoid function
-        sig_out = self.sig(out)
-
-        # reshape to be batch_size first
-        sig_out = sig_out.view(batch_size, -1)
-
-        sig_out = sig_out[:, -1]  # get last batch of labels
-
-        # return last sigmoid output and hidden state
-        return sig_out, hidden
-
-    def init_hidden(self, batch_size):
-        ''' Initializes hidden state '''
-        # Create two new tensors with sizes n_layers x batch_size x hidden_dim,
-        # initialized to zero, for hidden state and cell state of LSTM
-        h0 = torch.zeros((self.no_layers, batch_size, self.hidden_dim)).to(device)
-        c0 = torch.zeros((self.no_layers, batch_size, self.hidden_dim)).to(device)
-        hidden = (h0, c0)
-        return hidden
-
-    # function to predict accuracy
-
-
 def acc(pred, label):
     pred = torch.round(pred.squeeze())
     return torch.sum(pred == label.squeeze()).item()
 
 
 def main():
+    class SentimentRNN(nn.Module):
+        def __init__(self, no_layers, vocab_size, hidden_dim, embedding_dim, drop_prob=0.5):
+            super(SentimentRNN, self).__init__()
+
+            self.output_dim = output_dim
+            self.hidden_dim = hidden_dim
+
+            self.no_layers = no_layers
+            self.vocab_size = vocab_size
+
+            # embedding and LSTM layers
+            self.embedding = nn.Embedding(vocab_size, embedding_dim)
+
+            # lstm
+            self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=self.hidden_dim,
+                                num_layers=no_layers, batch_first=True)
+
+            # dropout layer
+            self.dropout = nn.Dropout(0.3)
+
+            # linear and sigmoid layer
+            self.fc = nn.Linear(self.hidden_dim, output_dim)
+            self.sig = nn.Sigmoid()
+
+        def forward(self, x, hidden):
+            batch_size = x.size(0)
+            # embeddings and lstm_out
+            embeds = self.embedding(x)  # shape: B x S x Feature   since batch = True
+            # print(embeds.shape)  #[50, 500, 1000]
+            lstm_out, hidden = self.lstm(embeds, hidden)
+
+            lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
+
+            # dropout and fully connected layer
+            out = self.dropout(lstm_out)
+            out = self.fc(out)
+
+            # sigmoid function
+            sig_out = self.sig(out)
+
+            # reshape to be batch_size first
+            sig_out = sig_out.view(batch_size, -1)
+
+            sig_out = sig_out[:, -1]  # get last batch of labels
+
+            # return last sigmoid output and hidden state
+            return sig_out, hidden
+
+        def init_hidden(self, batch_size):
+            ''' Initializes hidden state '''
+            # Create two new tensors with sizes n_layers x batch_size x hidden_dim,
+            # initialized to zero, for hidden state and cell state of LSTM
+            h0 = torch.zeros((self.no_layers, batch_size, self.hidden_dim)).to(device)
+            c0 = torch.zeros((self.no_layers, batch_size, self.hidden_dim)).to(device)
+            hidden = (h0, c0)
+            return hidden
+
+        # function to predict accuracy
+
     is_cuda = torch.cuda.is_available()
 
     # If we have a GPU available, we'll set our device to GPU. We'll use this device variable later in our code.
@@ -212,12 +211,14 @@ def main():
     epoch_tr_acc, epoch_vl_acc = [], []
 
     for epoch in range(epochs):
+        print(epoch)
         train_losses = []
         train_acc = 0.0
         model.train()
         # initialize hidden state
         h = model.init_hidden(batch_size)
         for inputs, labels in train_loader:
+            print(1)
             inputs, labels = inputs.to(device), labels.to(device)
             # Creating new variables for the hidden state, otherwise
             # we'd backprop through the entire training history
