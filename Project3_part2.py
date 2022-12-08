a301771eed0242d2be4be56cc3bdc3230450caca
daiyayuan <836919594@qq.com>
2022-12-08 18:01:20 +0800

    data clean finished

--- Project3_part2.py
+++ Project3_part2.py
@@ -1,7 +1,9 @@
+import re
+
+import nltk
 import numpy as np
 import pandas as pd
-import itertools
-
+from nltk.corpus import stopwords
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -13,10 +15,15 @@ from wordcloud import WordCloud
 def main():
     # 数据读取
     df = pd.read_csv('Project_Data/news.csv')
+    print(df.head().to_string())
     # 数据清洗
     df = data_clean(df)
-    word_cloud(df['text'].to_string())
+    # 取label列
     labels = df.label
+    print(df.head().to_string())
+    print(df['text'].head().to_string())
+    # 清洗后的数据生成词云
+    word_cloud(df['text'].to_string())
     # 数据集划分
     x_train, x_test, y_train, y_test = train_test_split(df['text'], labels, test_size=0.2, random_state=7)
     # 原始数据转TF-IDF
@@ -36,16 +43,27 @@ def main():
 
 # 数据清洗
 def data_clean(df):
+    # 删除空列
+    df = df.dropna()
+    # 根据text列进行去重
+    df.drop_duplicates(subset=['text'], keep='first', inplace=True)
+    # 去掉标点
+    p = re.compile(r'[^\w\s]+')
+    df['text'] = [p.sub('', x) for x in df['text'].tolist()]
+    # 去掉数字
+    p = re.compile(r'[^\D]+')
+    df['text'] = [p.sub('', x) for x in df['text'].tolist()]
+    # 去除停用词
+    stop_word = stopwords.words('english')
+    df['text'] = df['text'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop_word)]))
     return df
 
 
 # 生成词云
 def word_cloud(data):
-    print(data)
     plt.imshow(WordCloud(collocations=False, background_color='white', random_state=5726).generate(data),
                interpolation='bilinear')
     plt.savefig('news_word_cloud.png')
-    plt.show()
 
 
 if __name__ == '__main__':
